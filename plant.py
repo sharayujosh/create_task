6a6730cc42b6d80546fb38f65b34712bd4f39f8f
sharayujosh <61659558+sharayujosh@users.noreply.github.com>
2024-03-18 11:40:21 -0400

    i might go back to java haHa

--- plant.py
+++ plant.py
@@ -1,3 +1,20 @@
 import PySimpleGUI as gui
 
-gui.Window(title="Plant Watering Calendar", layout=[[]], margins=(100, 50)).read()
+# format = [[gui.Text("Hello from PySimpleGUI")], [gui.Button("OK")]]
+window = gui.Window(
+    background_color="light pink",
+    title="Plant Watering Calendar",
+    layout=[[gui.Text("Hello from PySimpleGUI")]],
+    margins=(100, 50),
+    icon="icon.png",
+).read()
+
+# Create an event loop
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the OK button
+    if event == gui.WIN_CLOSED:
+        break
+
+window.close()
