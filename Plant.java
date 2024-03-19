e5fcd7b32b9026eae16de215bbdd187d376962b1
sharayujosh <61659558+sharayujosh@users.noreply.github.com>
2024-03-19 18:10:54 +0000

    java

---
+++ Plant.java
@@ -0,0 +1,17 @@
+public class Plant{
+    public static void main(String[] args){
+        System.out.println("Welcome to the plant calendar!");
+        System.out.println("Possible commands: \n" +
+                            "N = New plant\n" +
+                            "D = Delete plant\n" +
+                            "I = Info on a specific plant\n" +
+                            "A = See all palnts\n" +
+                            "T = Which plants to water today\n" +
+                            "E = Exit");
+        action = 'E'
+        do{
+
+        }while(action != 'E')
+         = input("What would you like to do? ")
+    }
+}
--- plant.py
+++
@@ -1,3 +0,0 @@
-import PySimpleGUI as gui
-
-gui.Window(title="Plant Watering Calendar", layout=[[]], margins=(100, 50)).read()
