c891afa88b24c55b452547ad772c62e796e65b21
sharayujosh <61659558+sharayujosh@users.noreply.github.com>
2024-03-28 15:46:26 +0000

    trying json instead of csv

--- plant.py
+++ plant.py
@@ -1,4 +1,6 @@
-import csv
+# THIS CODE IS WRITTEN INDEPENDANTLY BY ME.
+
+import json
 
 # class Task:
 #     def __init__(self, name, unit, cycle, last_done):
@@ -10,54 +12,61 @@ import csv
 #         return f"{self.name}, last done {self.last_done}, must happen every {self.cycle} {self.cycle_units}"
 #     def __repr__(self):
 #         return self.__str__
+with open("schedule.txt") as schedule:
+    tasks = [schedule.read()]
 
-with open("schedule.csv", 'r') as schedule:
-    csvreader = csv.DictReader(schedule)
-    tasks = []
+    print(tasks)
 
-    for row in csvreader:
-        tasks.append(row)
-    
-    if len(tasks) == 0:
-        new = True
+with open("record.txt") as record:
+    records = [record.read()]
 
+    print(record)
 
-print(f"Welcome to the tasks tracker! You have {len(tasks)} tasks scheduled.")
+print(f"Welcome to the tasks tracker! You have {len(tasks)//2} tasks scheduled.")
 possible_commands = ['N', 'E', 'D', 'A', 'T', 'U', 'X']
-print("Possible commands: \nN = New task\nE = Edit task\nD = Delete task\nA = See all tasks\nT = Today's Tasks\nU = Upcoming tasks\nX = Exit")
+print("Possible commands: \nN = New task\nE = Edit record notes\nD = Delete task\nA = See all tasks\nT = Today's Tasks\nU = Upcoming tasks\nX = Exit")
 
+
+def see_all():
+    # print all plants, use repr
+    print("Tasks in schedule: ")
+    print(tasks)
 
 def add_new_task():
     # get necessary info, make new Plant, add Plant to list
     name = input("What is the name of your task? ")
-    unit = input("Is this task repeated every day, month, or year? ")
-    cycle = input(f"After how many {unit} should \"{name}\" be repeated? Enter an integer. ")
-    last_done = input(f"Enter the last time the task was completed in the format MM/DD/YYYY")
-    t = [name, unit, cycle, last_done]
-    with open("schedule.csv", 'a') as schedule:
-        csvwriter = csv.DictWriter(schedule, fieldnames=["name", "unit", "cycle", "last_done"])
-        if new:
-            csvwriter.writeheader()
-            new = False
-        csvwriter.writerow(t)
-        print("Task added.")
+    cycle = input(f"After how many days should \"{name}\" be repeated? Enter an integer. ")
+    t1 = {"name":name, "cycle":cycle}
+    tasks.append(t1)
+    # with open("schedule.txt", 'w') as schedule:
+    #     schedule.write(json.dumps(tasks))
+    last_done = input(f"Enter the last time the task was completed in the format MM/DD/YYYY: ")
+    notes = input(f"Notes: ")
+    t2 = {"name": name, "last_done":last_done, "notes":notes}
+    records.append(t2)
+    # with open("record.txt", 'w') as record:
+    #     record.write(json.dumps(record))
 
-def delete_plant():
-    # Use enumerate to print  plants in a list
+def delete_task():
+    # Use enumerate to print plants in a list
+    see_all()
+    key = input("Name of task to delete: ")
+    try:
+        del tasks[key]
+    except KeyError:
+        print("Invalid key. Must be EXACT.")
 
-    print("Which task would you like to delete?")
+def edit_record():
+    # Use enumerate to print plants in a list
+    see_all()
 
-def plant_info():
-    # Find the wanted plant, call to string of that plant
-    print("Which plant would you like to see? Identify by the corresponding number")
-
-def see_all():
-    # print all plants, use repr
-    print("Plants in database:")
-
-def to_water_today():
+def to_do_today():
     #print all plants to be watered today, include date
-    print("Plants to water today, insert-date-here :")
+    print("Tasks to do today, insert-date-here :")
+
+def upcoming():
+    #print all plants to be watered today, include date
+    print("Tasks to do today, insert-date-here :")
 
 # Find a way to make function that takes in a date and returns which plants need watering on that date
 
@@ -70,15 +79,22 @@ while(action != 'E'):
         continue
     
     if action == 'N':
-        add_new_plant()
+        add_new_task()
+    if action == 'E':
+        edit_record()
     if action == 'D':
-        delete_plant()
-    if action == 'I':
-        plant_info()
+        delete_task()
     if action == 'A':
         see_all()
     if action == 'T':
-        to_water_today()
-    if action == 'E':
+        to_do_today()
+    if action == 'U':
+        upcoming()
+    if action == 'X':
         print("Thanks for visiting :)")
         break
+
+    with open("schedule.txt", 'w') as schedule:
+        schedule.write(json.dumps(tasks))
+    with open("record.txt", 'w') as record:
+        record.write(json.dumps(records))
